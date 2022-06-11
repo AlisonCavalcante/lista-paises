d94120a9b0552a932de3624c1eb199decef04b4a
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-11 16:19:34 -0300

    create service país

---
+++ lista-paises/src/app/service/paises.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Constantes } from 'src/utils/Constantes';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaisesService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<any[]>{
+    return this.http.get<any>(Constantes.URL_ALL_PAISES);
+  }
+  getPais(nomePais: string): Observable<any>{
+    return this.http.get<any>(Constantes.URL_ALL_PAIS + `${nomePais}?fullText=true`);
+  }
+}
